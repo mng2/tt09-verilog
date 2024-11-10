f9ca85184ed200b44aef03ad3f4d2c358c3fa7c7
Mike Ng <mike@kj6aku.net>
2024-11-09 17:24:53 -0800

    test

--- src/project.v
+++ src/project.v
@@ -17,7 +17,9 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+    always @(posedge clk) begin
+        uo_out  <= ui_in[3:0] + uio_in[7:4];  // test
+    end
   assign uio_out = 0;
   assign uio_oe  = 0;
 
